82cfaab22756363680b0645f4fa76f7aa95abcef
FaysalXulhash <faysalahammed80@gmail.com>
2024-10-03 19:44:41 +0600

    fixed allowed host

--- puddle/settings.py
+++ puddle/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&-gun2)op$zmg$rea78$!qr(*6o&%v=asf-9ph$l%no+#(&qn7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['puddle-django-project.onrender.com']
 
 
 # Application definition
